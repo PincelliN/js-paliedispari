f1222953643bc0b189ddf4ad484c80cb9dfb7526
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-12 16:36:36 +0200

    add js Even or Odd function and path

--- B-exercise/js/Even and odd.js
+++ B-exercise/js/Even and odd.js
@@ -6,24 +6,67 @@ Dichiariamo chi ha vinto. */
 
 
 
+//variabile bottone
+const Button=document.querySelector("button");
+
+//Evento al click
+Button.addEventListener("click",function () {
+
+    //variabile pari o dispari
+    let EvenorOdd = displayRadioValue();
+    console.log(EvenorOdd);
+    //variabile Numero Utente
+    let UserNumber =document.getElementById("number").value;
+    console.log(UserNumber);
+    //variabile Numero Pc
+    let PcNumber=RandomNumber(1,5);
+    console.log(PcNumber);
+    //variabile somma
+    let ResultNumber = Somma(UserNumber,PcNumber);
+
+
+    if (EvenorOdd ==="Pari") {
+        let FinalResult = Even(ResultNumber);
+        console.log(FinalResult);
+        document.getElementById("result").innerHTML=FinalResult;
+    }else{
+        let FinalResult=Odd(ResultNumber);
+        console.log(FinalResult);
+         document.getElementById("result").innerHTML = FinalResult;
+    }
+    
+    
+})
+
+
+//Funzione somma
+function Somma(num1,num2) {
+    return  parseInt(num1) + parseInt(num2);
+}
 
 //Funzione pari
 function Even(element) {
-    let EventResult = element % 2 ===0 ? 'You Win':'You Lose';
-    return EventResult;
+    return  element % 2 ===0 ? 'You Win':'You Lose';
     
 }
 
 //Funzione dispari
 function Odd(element) {
-  let OddResult = element % 2 !== 0 ? "You Win" : "You Lose";
-  return OddResult;
+  return  element % 2 !== 0 ? "You Win" : "You Lose";
 }
 
 //Funzione genera numero da 1 a 5
 function RandomNumber(min,max) {
-    let RandomNum =Math.floor(Math.random()* (max - min + 1)+ min);
-    return RandomNum;
+   
+    return  Math.floor(Math.random()* (max - min + 1)+ min);
 }
 
 
+//estrazione del value del radio
+function displayRadioValue() {
+  var EvenOdd = document.getElementsByName("evenorodd");
+
+      let Choise = EvenOdd[0].checked ? 'Pari':'Dispari';
+
+  return Choise
+}

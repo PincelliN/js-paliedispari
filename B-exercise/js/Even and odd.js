1dfcd4a01230e677a64dba9fc75727cfe8d93fde
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-12 15:52:30 +0200

    add function randomNumber and functions Even or Odd

--- B-exercise/js/Even and odd.js
+++ B-exercise/js/Even and odd.js
@@ -3,3 +3,27 @@ L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
+
+
+
+
+//Funzione pari
+function Even(element) {
+    let EventResult = element % 2 ===0 ? 'You Win':'You Lose';
+    return EventResult;
+    
+}
+
+//Funzione dispari
+function Odd(element) {
+  let OddResult = element % 2 !== 0 ? "You Win" : "You Lose";
+  return OddResult;
+}
+
+//Funzione genera numero da 1 a 5
+function RandomNumber(min,max) {
+    let RandomNum =Math.floor(Math.random()* (max - min + 1)+ min);
+    return RandomNum;
+}
+
+
